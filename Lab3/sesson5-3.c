2ca13ea5d633ba9b9c2d7e223536d0a08e3e5175
xCirno1 <achicir2@gmail.com>
2020-09-16 18:15:17 +0700

    bai tap 5,6

---
+++ Lab3/sesson5-3.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main(void){
+	int iResult, a = 10, b = 8, c = 6, d = 5, e = 2;
+
+	iResult = a - b - c - d;
+	printf("iResult1 = a - b - c - d = %d\n",iResult);
+
+	iResult = a - b + c - d;
+	printf("iResult2 = a - b + c - d = %d\n",iResult);
+
+	iResult = a + b / c / d;
+	printf("iResult3 = a + b / c / d = %d\n",iResult);
+
+	iResult = a + b / c * d;
+	printf("iResult4 = a + b / c * d = %d\n",iResult);
+
+	iResult = a / b * c * d;
+	printf("iResult5 = a / b * c * d = %d\n",iResult);
+
+	iResult = a % b / c * d;
+	printf("iResult6 = a %% b / c * d = %d\n",iResult);
+
+	iResult = a % b % c % d;
+	printf("iResult7 = a %% b / c * d = %d\n",iResult);
+
+	iResult = a - (b - c) - d;
+	printf("iResult8 = a - (b - c) - d = %d\n",iResult);
+
+	iResult = (a - (b - c)) - d;
+	printf("iResult9 = (a - (b - c)) - d = %d\n",iResult);
+
+	iResult = a - (b - (c - d));
+	printf("iResult10 = a - (b - (c - d)) = %d\n",iResult);
+
+	iResult = a - ((b - c) - d);
+	printf("iResult11 = a - ((b - c) - d) = %d\n",iResult);
+
+	iResult = a % (b % c) * d * e;
+	printf("iResult12 = a %% (b %% c) * d * e = %d\n",iResult);
+
+	iResult = a + (b - c) * d - e;
+	printf("iResult13 = a + (b - c) * d - e = %d\n",iResult);
+
+	iResult = (a + b) * c + d * e;
+	printf("iResult14 = (a + b) * c + d * e = %d\n",iResult);
+
+	iResult = (a + b) * (c / d) % e;
+	printf("iResult15 = (a + b) * (c / d) %% e = %d\n",iResult);
+
+
+}
